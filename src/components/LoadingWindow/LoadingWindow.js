4af16d51ad140a755139305f97822b906456696a
park cho hye <chohye0831@gmail.com>
2025-01-15 22:34:14 +0900

    style : 로딩화면 서서히 사라지는 ui 구현

--- src/components/LoadingWindow/LoadingWindow.js
+++ src/components/LoadingWindow/LoadingWindow.js
@@ -1,15 +1,24 @@
 import './LoadingWindow.scss';
 import appleLogo from '../../resources/images/applelogo.png';
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 
 export default function LoadingWindow() {
+  useEffect(()=>{
+    const loadingWindow = document.getElementById('loading-window');
+    setTimeout(()=>{
+      loadingWindow.classList.remove('loading-window');
+      loadingWindow.classList.add('loading-window-hidden');
+    },3350)
+  })
+
   const animations = {
     initial: { width: "0px" },
     animate: { width: "150px" },
   };
   return (
-    <div className="loading-window">
+    <div className="loading-window" id='loading-window'>
       <img
       alt="logo"
       className="logo"
